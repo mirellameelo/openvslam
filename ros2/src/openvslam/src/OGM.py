36ddfd87a897b6c54d21176f4f433679c3e0776f
mirellameelo <miremelo@gmail.com>
2020-05-25 14:47:21 -0300

    generating 2d map and lines

--- ros2/src/openvslam/src/OGM.py
+++ ros2/src/openvslam/src/OGM.py
@@ -24,15 +24,23 @@ def save_data(scale_factor, quartile, path_to_3d_points, path_to_KF):
     x_3d = []
     y_3d = []
     z_3d = []
+
     # output 1: data treated. List: ( first keyframe id, x position, y position, z position )
     points_3d_data = []
 
     # vectors related to keyframe
     keyframe_id = []
+    KF_timestamp = []
     x_KF_position = []
     y_KF_position = []
     z_KF_position = []
-    KF_timestamp = []
+    x_KF = []
+    y_KF = []
+    z_KF = []
+    KF_quarternion_x = []
+    KF_quarternion_y = []
+    KF_quarternion_z = []
+    KF_quarternion_w = []
     # output 2: data treated. List: ( keyframe id, timestamp, x position, y position, z position )
     keyframe_data = []
 
@@ -46,7 +54,21 @@ def save_data(scale_factor, quartile, path_to_3d_points, path_to_KF):
         y_3d_position_raw.append(float(line_splited[2]) * scale_factor)
         z_3d_position_raw.append(float(line_splited[3]) * scale_factor)
 
-    # data treatment
+    # read keyframes data from txt
+    data_keyframe_related = open(path_to_KF).readlines()
+    for line in data_keyframe_related:
+        line_splited = line.split(' ')
+        keyframe_id.append(int(line_splited[0]))
+        KF_timestamp.append(float(line_splited[1]))
+        x_KF_position.append(float(line_splited[2]) * scale_factor)
+        y_KF_position.append(float(line_splited[3]) * scale_factor)
+        z_KF_position.append(float(line_splited[4]) * scale_factor)
+        KF_quarternion_x.append(float(line_splited[5]))
+        KF_quarternion_y.append(float(line_splited[6]))
+        KF_quarternion_z.append(float(line_splited[7]))
+        KF_quarternion_w.append(float(line_splited[8]))
+        
+    # 3D points data treatment
     threshhold_x_sup = np.mean(x_3d_position_raw) - quartile * np.std(x_3d_position_raw)
     threshhold_x_inf = np.mean(x_3d_position_raw) + quartile * np.std(x_3d_position_raw)
 
@@ -57,7 +79,7 @@ def save_data(scale_factor, quartile, path_to_3d_points, path_to_KF):
     threshhold_z_inf = np.mean(z_3d_position_raw) + quartile * np.std(z_3d_position_raw)
 
     # save 3d point data treated 
-    for i in xrange(len(x_3d_position_raw)):
+    for i in range(len(x_3d_position_raw)):
         if x_3d_position_raw[i] > threshhold_x_sup and  y_3d_position_raw[i] > threshhold_y_sup and z_3d_position_raw[i] > threshhold_z_sup:
             if x_3d_position_raw[i] < threshhold_x_inf and y_3d_position_raw[i] < threshhold_y_inf and z_3d_position_raw[i] < threshhold_z_inf:
                 keyframe_point_id.append(first_keyframe_id[i])
@@ -66,9 +88,18 @@ def save_data(scale_factor, quartile, path_to_3d_points, path_to_KF):
                 z_3d_position.append(z_3d_position_raw[i])
 
     # shifting x and z axis to origin sistem coordinator
-    shift_x = abs(min(x_3d_position))
-    shift_y = abs(min(y_3d_position))
-    shift_z = abs(min(z_3d_position))
+    shift_x_point = min(x_3d_position)
+    shift_y_point = min(y_3d_position)
+    shift_z_point = min(z_3d_position)
+    
+    shift_x_KF = min(x_KF_position)
+    shift_y_KF = min(y_KF_position)
+    shift_z_KF = min(z_KF_position)
+
+    shift_x = abs(min(shift_x_point, shift_x_KF))
+    shift_y = abs(min(shift_y_point, shift_y_KF))
+    shift_z = abs(min(shift_z_point, shift_z_KF))
+
     for x_3d_position in x_3d_position:
         x_3d.append(x_3d_position + shift_x)
     for y_3d_position in y_3d_position:
@@ -76,23 +107,22 @@ def save_data(scale_factor, quartile, path_to_3d_points, path_to_KF):
     for z_3d_position in z_3d_position:
         z_3d.append(z_3d_position + shift_z)
 
-    # save 3d point data treated
-    for i in xrange(len(x_3d)):
-        points_3d_data.append((keyframe_point_id[i], x_3d[i] , y_3d[i], z_3d[i]))
+    for x_KF_position in x_KF_position:
+        x_KF.append(x_KF_position + shift_x)
+    for y_KF_position in y_KF_position:
+        y_KF.append(y_KF_position + shift_y)
+    for z_KF_position in z_KF_position:
+        z_KF.append(z_KF_position + shift_z)
 
-    #read keyframes data from txt
-    data_keyframe_related = open(path_to_KF).readlines()
-    for line in data_keyframe_related:
-        line_splited = line.split(' ')
-        keyframe_id.append(int(line_splited[0]))
-        KF_timestamp.append(float(line_splited[1]))
-        x_KF_position.append(float(line_splited[2]) * scale_factor + shift_x)
-        y_KF_position.append(float(line_splited[3]) * scale_factor + shift_y)
-        z_KF_position.append(float(line_splited[4]) * scale_factor + shift_z)
+    # save 3d point data treated
+    for i in range(len(x_3d)):
+        points_3d_data.append([keyframe_point_id[i], x_3d[i] , y_3d[i], z_3d[i]])
 
     # save keyframe data treated 
-    for i in xrange(len(x_KF_position)):
-        keyframe_data.append(( keyframe_id[i], KF_timestamp[i], x_KF_position[i] , y_KF_position[i], z_KF_position[i]))
+    for i in range(len(x_KF)):
+        keyframe_data.append([ keyframe_id[i], KF_timestamp[i], x_KF[i], 
+                                y_KF[i], z_KF[i], KF_quarternion_x[i], 
+                                KF_quarternion_y[i], KF_quarternion_z[i], KF_quarternion_w[i] ])
 
     #return 3d point data and keyframe data
     return points_3d_data, keyframe_data
@@ -112,7 +142,97 @@ def draw_2d_point_cloud(points_3d_data, keyframe_data):
     z_keyframe = []
 
     # saving 2d points position
-    for i in xrange(len(points_3d_data)):
+    for i in range(len(points_3d_data)):
+        x_3d.append(points_3d_data[i][1])
+        z_3d.append(points_3d_data[i][3])
+
+    # OGM windown size taking into consideration the scale factor
+    #tem q mudar isso, p ser o max entre KF e points
+    windown_size_x = int(max(x_3d)) + 10
+    windown_size_z = int(max(z_3d)) + 10
+    img = [windown_size_x, windown_size_z]
+    img = np.zeros((windown_size_x, windown_size_z, 3), np.uint8)
+
+    # drawing 2d points position in red color
+    for i in range(len(x_3d)):    
+        img[int(x_3d[i]),int(z_3d[i])] = (0,0, 255)   
+
+    # saving vectors to 2d keyframe position
+    for i in range(len(keyframe_data)):
+        x_keyframe.append(keyframe_data[i][2])
+        z_keyframe.append(keyframe_data[i][4])
+
+    # drawing 2d keyframe positon in light blue color
+    for i in range(len(x_keyframe)):
+        img[int(x_keyframe[i]), int(z_keyframe[i])] = (255, 255, 0)
+
+    return img
+
+
+
+def draw_2d_keyframes(points_3d_data, keyframe_data):
+
+    print('Use the output data from save_data() function.')
+
+    # vectors related to 2d points position
+    x_3d = []
+    z_3d = []
+
+    # vectors related to 2d keyframe position
+    x_keyframe = []
+    z_keyframe = []
+
+    # saving 2d points position
+    for i in range(len(points_3d_data)):
+        x_3d.append(points_3d_data[i][1])
+        z_3d.append(points_3d_data[i][3])
+
+    # OGM windown size taking into consideration the scale factor
+    windown_size_x = int(max(x_3d)) + 10
+    windown_size_z = int(max(z_3d)) + 10
+    img = [windown_size_x, windown_size_z]
+    img = np.zeros((windown_size_x, windown_size_z, 3), np.uint8)
+
+    # saving vectors to 2d keyframe position
+    for i in range(len(keyframe_data)):
+        x_keyframe.append(keyframe_data[i][2])
+        z_keyframe.append(keyframe_data[i][4])
+
+    # drawing 2d keyframe positon in light blue color
+    #for i in range(len(x_keyframe)):
+    #    img[int(x_keyframe[i]), int(z_keyframe[i])] = (255, 255, 0)
+    
+    kernel = np.ones((5,5),np.uint8)
+    img = cv2.erode(img,kernel,iterations = 1)
+    img = cv2.dilate(img,kernel,iterations = 1)
+
+    return img
+
+
+
+def filter_ground_points(points_3d_data, threshhold):
+
+    print('Use the output data from save_data() function.')
+    ground = []
+    for i in range(len(points_3d_data)):
+        if points_3d_data[i][2] < threshhold:
+            ground.append(points_3d_data[i])
+    
+    return ground
+
+
+def draw_lines(point_2d, keyframe, lista):
+    
+     # vectors related to 2d points position
+    x_3d = []
+    z_3d = []
+
+    # vectors related to 2d keyframe position
+    x_keyframe = []
+    z_keyframe = []
+
+    # saving 2d points position
+    for i in range(len(points_3d_data)):
         x_3d.append(points_3d_data[i][1])
         z_3d.append(points_3d_data[i][3])
 
@@ -123,22 +243,58 @@ def draw_2d_point_cloud(points_3d_data, keyframe_data):
     img = np.zeros((windown_size_x, windown_size_z, 3), np.uint8)
 
     # drawing 2d points position in red color
-    for i in xrange(len(x_3d)):    
+    for i in range(len(lista)):    
         img[int(x_3d[i]),int(z_3d[i])] = (0,0, 255)   
 
-    # saving vectors to 2d keyframe position
-    for i in xrange(len(keyframe_data)):
-        x_keyframe.append(keyframe_data[i][2])
-        z_keyframe.append(keyframe_data[i][4])
+    return img
 
-    print(len(z_keyframe))
-    print(len(x_keyframe))
-    print(len(x_3d))
-    print(len(z_3d))
-    # drawing 2d keyframe positon in light blue color
-    for i in xrange(len(x_keyframe)):
-        img[int(x_keyframe[i]), int(z_keyframe[i])] = (255, 255, 0)
+def get_line_bresenham(start, end):
+    """Bresenham's Line Algorithm
+    Produces a list of tuples from start and end
+    """
+    # Setup initial conditions
+    x1, y1 = start
+    x2, y2 = end
+    dx = x2 - x1
+    dy = y2 - y1
 
-    # showing the image result
-    cv2.imshow('3D points and keyframes.', img)
-    cv2.waitKey(0)
+    #
+    # Determine how steep the line is
+    is_steep = abs(dy) > abs(dx) #output: true or false
+
+    # Rotate line
+    if is_steep: #if true, swap start and end points
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    # Swap start and end points if necessary and store swap state
+    swapped = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        swapped = True
+
+    # Recalculate differentials
+    dx = x2 - x1
+    dy = y2 - y1
+
+    # Calculate error
+    error = int(dx / 2.0)
+    ystep = 1 if y1 < y2 else -1
+
+    # Iterate over bounding box generating points between start and end
+    y = y1
+    points = []
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if is_steep else (x, y)
+        points.append(coord)
+        error -= abs(dy)
+        if error < 0:
+            y += ystep
+            error += dx
+
+    # Reverse the list if the coordinates were swapped
+    if swapped:
+        points.reverse()
+
+    return points #return the points that indicate the LINE
--- ros2/src/openvslam/src/teste_OGM.py
+++ ros2/src/openvslam/src/teste_OGM.py
@@ -1,4 +1,76 @@
 import OGM as ogm
+import cv2
+import numpy as np
 
-points_2d, keyframe = ogm.save_data(100, 1.5, './../../../maps/landmarks_and_timestamp.txt', './../../../maps/keyframe_trajectory.txt')
-ogm.draw_2d_point_cloud(points_2d, keyframe)
+points, keyframe = ogm.save_data(100, 1.5, './../../../maps/landmarks_and_timestamp.txt', './../../../maps/keyframe_trajectory.txt')
+
+points_2d = ogm.filter_ground_points(points, 40)
+img = ogm.draw_2d_point_cloud(points, keyframe)
+
+
+kf_and_points = []
+x = []
+z = []
+
+a = []
+a = np.shape(img)
+
+kernel = np.ones((5,5),np.uint8)
+img = cv2.erode(img,kernel,iterations = 1)
+img = cv2.dilate(img,kernel,iterations = 1)
+
+#img = np.zeros((a[0], a[1], 3), np.uint8)
+
+for i in range(len(points_2d)):
+    x.append(points_2d[i][2])
+    z.append(points_2d[i][3])
+
+for i in range(len(keyframe)):
+    KF_id = keyframe[i][0]
+    for a in range(len(points_2d)):
+        if points_2d[a][0] == KF_id:
+            c = int(keyframe[i][2])
+            d = int(keyframe[i][4])
+            e = int(points_2d[a][1])
+            f = int(points_2d[a][3])
+            ray_points = ogm.get_line_bresenham( [c, d], [e,f] )
+            #print(ray_points)
+            for b in range(len(ray_points)):
+                img[ray_points[b][0], ray_points[b][1]] = (105, 255, 100)
+
+
+#kernel = np.ones((5,5),np.uint8)
+#img = cv2.erode(img,kernel,iterations = 1)
+#img = cv2.dilate(img,kernel,iterations = 1)
+
+cv2.imshow('hello hello remember me?', img)
+cv2.waitKey(0)
+
+# for i in range(len(points_2d)):
+#     x.append(points_2d[i][2])
+#     z.append(points_2d[i][3])
+#     
+# norm_factor_x = float(max(x) - 1) / float(max(x))
+# norm_factor_z = float(max(z) - 1) / float(max(z))
+
+
+
+# for i in range(len(keyframe)):
+#     KF_id = keyframe[i][0]
+#     for a in range(len(points_2d)):
+#         if points_2d[a][0] == KF_id:
+#             c = int(keyframe[i][2])
+#             d = int(keyframe[i][4])
+#             e = int(points_2d[a][1])
+#             f = int(points_2d[a][3])
+#             ray_points = ogm.get_line_bresenham( [c, d], [e,f] )
+#             n_ray_pts = len(ray_points)
+#             for ray_point_id in range(n_ray_pts - 1):
+#                 ray_point_x_norm = int(np.floor((ray_points[ray_point_id][0]) * norm_factor_x))
+#                 ray_point_z_norm = int(np.floor((ray_points[ray_point_id][1]) * norm_factor_z))
+#             ray_point_x_norm = int(np.floor((ray_points[-1][0]) * norm_factor_x))
+#             ray_point_z_norm = int(np.floor((ray_points[-1][1]) * norm_factor_z))
+#             #p deveria ser igual ao comprimento de point_2s
+#             p = p+1
+#             print(ray_points)
+
