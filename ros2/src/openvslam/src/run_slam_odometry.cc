5db7ac95274e038827845db0d911145c39081e31
mirellameelo <miremelo@gmail.com>
2020-06-16 05:04:46 -0300

    publishing odometry data

--- ros2/src/openvslam/src/run_slam_odometry.cc
+++ ros2/src/openvslam/src/run_slam_odometry.cc
@@ -1,24 +1,30 @@
+#ifdef USE_PANGOLIN_VIEWER
+#include <pangolin_viewer/viewer.h>
+#elif USE_SOCKET_PUBLISHER
+#include <socket_publisher/publisher.h>
+#endif
+
 #include <openvslam/system.h>
 #include <openvslam/config.h>
-#include "openvslam/publish/map_publisher.h"
 
 #include <iostream>
 #include <chrono>
 #include <numeric>
 
 #include <rclcpp/rclcpp.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-
-#include <tf2_ros/transform_listener.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <spdlog/spdlog.h>
 #include <popl.hpp>
+
+// ROS packages
+#include <nav_msgs/msg/odometry.hpp>
+#include "geometry_msgs/msg/pose.hpp"
+#include <tf2_ros/transform_listener.h>
 
 #ifdef USE_STACK_TRACE_LOGGER
 #include <glog/logging.h>
@@ -27,6 +33,46 @@
 #ifdef USE_GOOGLE_PERFTOOLS
 #include <gperftools/profiler.h>
 #endif
+
+auto publi(auto cam_pose_, auto odometry_pub_, auto node){
+    Eigen::Matrix3d rotation_matrix = cam_pose_.block(0, 0, 3, 3);
+    Eigen::Vector3d translation_vector = cam_pose_.block(0, 3, 3, 1);
+
+    tf2::Matrix3x3 tf_rotation_matrix(rotation_matrix(0, 0), rotation_matrix(0, 1), rotation_matrix(0, 2),
+                                      rotation_matrix(1, 0), rotation_matrix(1, 1), rotation_matrix(1, 2),
+                                      rotation_matrix(2, 0), rotation_matrix(2, 1), rotation_matrix(2, 2));
+
+    tf2::Vector3 tf_translation_vector(translation_vector(0), translation_vector(1), translation_vector(2));
+
+    //Coordinate transformation matrix from slam coordinate system to ros coordinate system
+    tf2::Matrix3x3 tf_open_to_ros (0, 0, 1,
+                                 -1, 0, 0,
+                                 0,-1, 0);
+
+    //Transform actual coordinate system to ros coordinate system on camera coordinates
+    tf_rotation_matrix = tf_open_to_ros*tf_rotation_matrix;
+    tf_translation_vector = tf_open_to_ros*tf_translation_vector;
+
+    tf_rotation_matrix = tf_rotation_matrix.transpose();
+    tf_translation_vector = -(tf_rotation_matrix*tf_translation_vector);
+
+    tf2::Transform transform_tf(tf_rotation_matrix, tf_translation_vector);
+
+    // Create odometry message and update it with current camera pose
+    nav_msgs::msg::Odometry odom_msg_;
+    odom_msg_.header.stamp = node->now();
+    odom_msg_.header.frame_id = "camera_frame";
+    odom_msg_.child_frame_id = "base_link_frame";
+    odom_msg_.pose.pose.orientation.x = transform_tf.getRotation().getX();
+    odom_msg_.pose.pose.orientation.y = transform_tf.getRotation().getY();
+    odom_msg_.pose.pose.orientation.z = transform_tf.getRotation().getZ();
+    odom_msg_.pose.pose.orientation.w = transform_tf.getRotation().getW();
+
+    odom_msg_.pose.pose.position.x = transform_tf.getOrigin().getX();
+    odom_msg_.pose.pose.position.y = transform_tf.getOrigin().getY();
+    odom_msg_.pose.pose.position.z = transform_tf.getOrigin().getZ();
+    odometry_pub_->publish(odom_msg_);
+}
 
 void mono_tracking(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path,
                    const std::string& mask_img_path, const bool eval_log, const std::string& map_db_path) {
@@ -38,6 +84,14 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg, const std::str
     // startup the SLAM process
     SLAM.startup();
 
+    // create a viewer object
+    // and pass the frame_publisher and the map_publisher
+#ifdef USE_PANGOLIN_VIEWER
+    pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#elif USE_SOCKET_PUBLISHER
+    socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#endif
+
     std::vector<double> track_times;
     const auto tp_0 = std::chrono::steady_clock::now();
 
@@ -45,10 +99,9 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg, const std::str
     auto node = std::make_shared<rclcpp::Node>("run_slam");
     rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
     custom_qos.depth = 1;
-
-
-    // publisher
+    // declare publisher
     auto odometry_pub_ = node->create_publisher<nav_msgs::msg::Odometry>("odometry", 1);
+
 
     // run the SLAM as subscriber
     image_transport::Subscriber sub = image_transport::create_subscription(
@@ -57,70 +110,52 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg, const std::str
             const auto timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(tp_1 - tp_0).count();
 
             // input the current frame and estimate the camera pose
-            SLAM.feed_monocular_frame(cv_bridge::toCvShare(msg, "bgr8")->image, timestamp, mask);
+            auto cam = SLAM.feed_monocular_frame(cv_bridge::toCvShare(msg, "bgr8")->image, timestamp, mask);
 
             const auto tp_2 = std::chrono::steady_clock::now();
 
             const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
             track_times.push_back(track_time);
+            publi(cam, odometry_pub_, node);
         },
         "raw", custom_qos);
 
     rclcpp::executors::SingleThreadedExecutor exec;
     exec.add_node(node);
 
-    std::shared_ptr<openvslam::publish::map_publisher> map_publisher_ = SLAM.get_map_publisher();
+    // Pangolin needs to run in the main thread on OSX
+    std::thread thread([&]() {
+        exec.spin();
+    });
 
-    rclcpp::WallRate pub_rate(10);
-    auto cam_pose_ = map_publisher_->get_current_cam_pose();
-
-    while (rclcpp::ok()) {
-        // SLAM get the motion matrix publisher
-        cam_pose_ = map_publisher_->get_current_cam_pose();
-
-        // Extract rotation matrix and translation vector from 
-        Eigen::Matrix3d rotation_matrix = cam_pose_.block(0, 0, 3, 3);
-        Eigen::Vector3d translation_vector = cam_pose_.block(0, 3, 3, 1);
-
-        tf2::Matrix3x3 tf_rotation_matrix(rotation_matrix(0, 0), rotation_matrix(0, 1), rotation_matrix(0, 2),
-                                          rotation_matrix(1, 0), rotation_matrix(1, 1), rotation_matrix(1, 2),
-                                          rotation_matrix(2, 0), rotation_matrix(2, 1), rotation_matrix(2, 2));
-
-        tf2::Vector3 tf_translation_vector(translation_vector(0), translation_vector(1), translation_vector(2));
-
-        tf2::Transform transform_tf(tf_rotation_matrix, tf_translation_vector);
-
-        // Create odometry message and update it with current camera pose
-        nav_msgs::msg::Odometry odom_msg_;
-        
-        odom_msg_.header.stamp = node->now();
-        odom_msg_.header.frame_id = "camera_frame";
-        odom_msg_.child_frame_id = "base_link_frame";
-
-        odom_msg_.pose.pose.orientation.x = transform_tf.getRotation().getX();
-        odom_msg_.pose.pose.orientation.y = transform_tf.getRotation().getY();
-        odom_msg_.pose.pose.orientation.z = transform_tf.getRotation().getZ();
-        odom_msg_.pose.pose.orientation.w = transform_tf.getRotation().getW();
-
-        odom_msg_.pose.pose.position.x = transform_tf.getOrigin().getX();
-        odom_msg_.pose.pose.position.y = transform_tf.getOrigin().getY();
-        odom_msg_.pose.pose.position.z = transform_tf.getOrigin().getZ();
-
-        //Coordinate transformation matrix from slam coordinate system to ros coordinate system
-        tf2::Matrix3x3 tf_open_to_ros (0, 0, 1,
-                                    -1, 0, 0,
-                                    0,-1, 0);
-
-        //Transform actual coordinate system to ros coordinate system on camera coordinates
-        tf_rotation_matrix = tf_open_to_ros*tf_rotation_matrix;
-        tf_translation_vector = tf_open_to_ros*tf_translation_vector;
-
-        tf_rotation_matrix = tf_rotation_matrix.transpose();
-
-        odometry_pub_->publish(odom_msg_);
-        exec.spin_some();
-        pub_rate.sleep();
+#ifdef USE_PANGOLIN_VIEWER
+    viewer.run();
+    if (SLAM.terminate_is_requested()) {
+        // wait until the loop BA is finished
+        while (SLAM.loop_BA_is_running()) {
+            std::this_thread::sleep_for(std::chrono::microseconds(5000));
+        }
+        rclcpp::shutdown();
     }
+#elif USE_SOCKET_PUBLISHER
+    publisher.run();
+    if (SLAM.terminate_is_requested()) {
+        // wait until the loop BA is finished
+        while (SLAM.loop_BA_is_running()) {
+            std::this_thread::sleep_for(std::chrono::microseconds(5000));
+        }
+        rclcpp::shutdown();
+    }
+#endif
+
+    // automatically close the viewer
+#ifdef USE_PANGOLIN_VIEWER
+    viewer.request_terminate();
+    thread.join();
+#elif USE_SOCKET_PUBLISHER
+    publisher.request_terminate();
+    thread.join();
+#endif
 
     // shutdown the SLAM process
     SLAM.shutdown();
@@ -168,7 +203,6 @@ int main(int argc, char* argv[]) {
     auto debug_mode = op.add<popl::Switch>("", "debug", "debug mode");
     auto eval_log = op.add<popl::Switch>("", "eval-log", "store trajectory and tracking times for evaluation");
     auto map_db_path = op.add<popl::Value<std::string>>("", "map-db", "store a map database at this path after SLAM", "");
-
     try {
         op.parse(argc, argv);
     }
